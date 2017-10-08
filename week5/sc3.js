b95471017adbcb5f6e423210a80b90a37af5720c
temirlansh <31761424+temirlansh@users.noreply.github.com>
2017-10-08 12:54:40 +0600

    Add week5

--- week5/sc3.js
+++ week5/sc3.js
@@ -2,6 +2,15 @@ function add(){
 	var name = document.getElementById("name").value;
 	var surname = document.getElementById("surname").value;
 	var fac = document.getElementById("faculty").value;
+
+	if(name==""){
+		document.getElementById("name").style.border="3px solid red";
+		return;
+			}
+    if(surname==""){
+		document.getElementById("surname").style.border="3px solid red";
+		return;
+			}
 	
 
 	var tr = document.createElement("tr");
@@ -12,6 +21,7 @@ function add(){
 	td1.innerHTML = name;
 	td2.innerHTML = surname;
 	td3.innerHTML = "<strong>" + fac + "</strong>";
+
 
 	tr.appendChild(td1);
 	tr.appendChild(td2);
